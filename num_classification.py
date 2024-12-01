56385f85c9d07d6d36bc3526cebf555fe3163ed0
areloyann <araqseloyan23@gmail.com>
2024-12-01 13:45:50 +0400

    Added new updates

---
+++ num_classification.py
@@ -0,0 +1,15 @@
+def classify_number(numbers):
+    even_numbers=[]
+    odd_numbers=[]
+    for num in numbers:
+        if num%2==0:
+            even_numbers.append(num)
+        else:
+            odd_numbers.append(num)
+    return even_numbers, odd_numbers
+if __name__== "__main__":
+    user_input = input("Enter a list of numbers separated by spaces (e.g., '1 2 3 4 5'): ")
+    number_list=[int(x) for x in user_input.split()]
+    even_numbers, odd_numbers = classify_number(number_list)
+    print("Even numbers:", even_numbers)
+    print("Odd numbers:", odd_numbers)
